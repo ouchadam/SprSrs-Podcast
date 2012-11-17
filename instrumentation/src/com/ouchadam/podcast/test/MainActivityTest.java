c40aba95c3a00d64d7ba65d9e1f234d31ae9f1d3
Adam Brown <adam@novoda.com>
2012-11-17 15:57:56 +0000

    init project

---
+++ instrumentation/src/com/ouchadam/podcast/test/MainActivityTest.java
@@ -0,0 +1,17 @@
+package com.ouchadam.podcast.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.ouchadam.podcast.*;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    public void testActivity() {
+        MainActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
