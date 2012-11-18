b4ad8b9c351bdba431138a4515b34e370a60df4c
Adam Brown <adam@novoda.com>
2012-11-18 23:43:05 +0000

    added more structure

---
+++ app/src/com/ouchadam/podcast/fragment/DownloadsFragment.java
@@ -0,0 +1,6 @@
+package com.ouchadam.podcast.fragment;
+
+import com.actionbarsherlock.app.SherlockListFragment;
+
+public class DownloadsFragment extends SherlockListFragment {
+}
---
+++ app/src/com/ouchadam/podcast/fragment/LatestItemsFragment.java
@@ -0,0 +1,6 @@
+package com.ouchadam.podcast.fragment;
+
+import com.actionbarsherlock.app.SherlockListFragment;
+
+public class LatestItemsFragment extends SherlockListFragment {
+}
