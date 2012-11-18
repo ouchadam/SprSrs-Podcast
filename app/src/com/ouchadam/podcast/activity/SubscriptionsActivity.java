6c7dd6e201765916601d499486f36ba660937c9a
Adam Brown <adam@novoda.com>
2012-11-18 00:16:45 +0000

    building up app structure

--- app/src/com/ouchadam/podcast/activity/SubscriptionsActivity.java
+++ app/src/com/ouchadam/podcast/activity/SubscriptionsActivity.java
@@ -1,4 +1,14 @@
 package com.ouchadam.podcast.activity;
 
-public class SubscriptionsActivity {
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.ouchadam.podcast.R;
+
+public class SubscriptionsActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_list);
+    }
 }
